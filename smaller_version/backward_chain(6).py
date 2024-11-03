742875582c92144b693f0961ebfa978eaad64981
Shreevathsatantry <shreevathsatantry@gmail.com>
2024-11-03 21:22:26 +0530

    commit

--- smaller_version/backward_chain(6).py
+++ smaller_version/backward_chain(6).py
@@ -1,14 +1,21 @@
-db = ["Croaks", "Eat Flies"]
+db = ["Croaks", "Eat Flies", "Shrimps", "Sings"]
 kb = ["Frog", "Canary"]
-color = ["Green", "Yellow"]
+cl = ["Green", "Yellow"]
 
 def main():
-    x = int(input("*-----Backward Chaining*\n X is \n1. Frog \n2. Canary\nSelect One: "))
-    if x in (1, 2): print(f"Chance Of {'eating flies' if x == 1 else 'shrimping'}")
-    else: return print("\n-------Invalid Option Select")
+    x = int(input("*-----Backward Chaining*\n X is\n1. Frog \n2. Canary\nSelect One: "))
+    if x in (1, 2): 
+        print(f"Chance of {db[x - 1]} Color Is 1. Green 2. Yellow")
+    else: 
+        return print("\n-------Invalid Option Select")
 
-    k = int(input(f"\n X is {kb[x - 1]}\n1. Green \n2. Yellow\nSelect Option: "))
-    if k in (1, 2): print(f"Yes, it is in {color[k - 1]} colour and will {db[x - 1]}")
-    else: print("\n---Invalid Knowledge Database")
+    k = int(input("Select Color: "))
+    if (k == 1 and x == 1): 
+        print(f"Yes, it is {kb[0]} And Color Is {cl[0]}")
+    elif (k == 2 and x == 2): 
+        print(f"Yes, it is {kb[1]} And Color Is {cl[1]}")
+    else: 
+        print("\n---Invalid Knowledge Database")
 
-if __name__ == "__main__": main()
+if __name__ == "__main__": 
+    main()
